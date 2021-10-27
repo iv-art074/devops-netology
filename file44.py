b12ce2d4c63d5594b741987f92aa717022085e6e
iv-art074 <iv_art@mail.ru>
2021-10-28 00:18:11 +1000

    mypylab 1

--- file44.py
+++ file44.py
@@ -3,10 +3,10 @@ import yaml
 import json
 from pprint import pprint
 
-with open('file1.json', encoding = 'utf-8') as f:
-    data: Any=json.load(f)
-    pprint (data)
+with open('file1.json', encoding='utf-8') as f:
+    data: Any = json.load(f)
+    pprint(data)
 
 with open('file1.yaml', encoding='utf-8') as fy:
-    datay: Any = yaml.load(fy,Loader=yaml.FullLoader)
+    datay: Any = yaml.load(fy, Loader=yaml.FullLoader)
     pprint(datay)
