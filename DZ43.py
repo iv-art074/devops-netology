58642dd12651b9c5c7870605909ef5c402169bb9
iv-art074 <iv_art@mail.ru>
2021-10-27 21:40:13 +1000

    mypylab

--- DZ43.py
+++ DZ43.py
@@ -1,19 +1,21 @@
+from typing import Any
+
 import socket as sckt
 import time as time
 import json
 import yaml
 
 srv_lst = {'mail.google.com':'0.0.0.0', 'drive.google.com':'0.0.0.0','google.com':'0.0.0.0'}
-# fpath = "/home/alex/python/"
+
 
 while True : #бесконечный цикл
   data=[]
+  hosts: Any
   for hosts in srv_lst:
     ip = sckt.gethostbyname(hosts)
     if ip != srv_lst[hosts]:
       with open('file1.json', 'a') as js:
         print('[изменен] ' + str(hosts) +' IP mistmatch: '+srv_lst[hosts]+' '+ip)
-      # data.append({host:ip})
         json_data= json.dumps({hosts:ip})
         js.write(json_data)
       with open('file1.yaml', 'a') as ym:
