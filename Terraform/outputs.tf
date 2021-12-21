07e87cf6511732f19bade00fc332c4715eec2efc
iv-art074 <87374285+iv-art074@users.noreply.github.com>
2021-12-21 17:16:11 +1000

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -6,3 +6,12 @@ output "caller_user" {
   value = data.aws_caller_identity.current.user_id  
 }  
 
+
+output "instance_ip_addr" {
+  value = aws_instance.server.private_ip
+}
+
+output "vpc_id" {
+  description = "ID of project VPC"
+  value       = module.vpc.vpc_id
+}
