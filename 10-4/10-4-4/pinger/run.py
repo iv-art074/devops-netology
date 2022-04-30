79d8811fe2df4c60b3bd1009d4cbe316b1fa0db3
root <root@Pappa.localdomain>
2022-04-30 12:16:58 +1000

    try other vers

--- 10-4/10-4-4/pinger/run.py
+++ 10-4/10-4-4/pinger/run.py
@@ -3,16 +3,11 @@
 import logging
 import random
 import time
-import datetime
 
-#dt_now = datetime.datetime.now()
-
-#logging.basicConfig(filename='./logs/file.log',format='%(asctime)s,%(levelname)s ,%(message)s',datefmt='%Y-%m-%dT%H:%M:%S')
-#logging.basicConfig(filename='./logs/file.log',format='{\"level\":\"%(levelname)s\",\"message\":\"%(message)s\"}')
-logging.basicConfig(filename='./logs/file.log')
 while True:
 
     number = random.randrange(0, 4)
+
     if number == 0:
         logging.info('Hello there!!')
     elif number == 1:
@@ -22,19 +17,4 @@ while True:
     elif number == 3:
         logging.exception(Exception('this is exception'))
 
-#    dt_now = datetime.datetime.now()
     time.sleep(1)
-"""    if number == 0:
-        logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-        logging.info('Hello there!!')
-    elif number == 1:
-        logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.WARNING)
-        logging.warning('Hmmm....something strange')
-    elif number == 2:
-        logging.basicConfig(format='%(asctime)s - %(message)s',level=logging.ERROR)
-        logging.error('OH NO!!!!!!')
-    elif number == 3:
-        logging.basicConfig(format='%(asctime)s - %(message)s',level=logging.DEBUG)
-        logging.exception(Exception('this is exception'))
-"""
-
